c31ea791d6b91e987a9dc38373d3e392133fdcb7
wat <skarndwlsdud801@gmail.com>
2024-11-12 17:38:34 +0900

    f

---
+++ src/programming/BOJ_1001.java
@@ -0,0 +1,17 @@
+package programming;
+
+import java.util.Scanner;
+
+public class BOJ_1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a > 0 && b < 10){
+            System.out.println(a-b);
+        }else{
+            System.out.println("다시 입력");
+        }
+
+    }
+}
