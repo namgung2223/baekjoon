c9cdf0aa11a1127f64c1b73da29220bd85515996
wat <skarndwlsdud801@gmail.com>
2025-01-08 16:34:33 +0900

    f

---
+++ src/data_structure/BOJ_1927.java
@@ -0,0 +1,33 @@
+package data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ_1927 {
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //연산의 개수
+        int N = Integer.parseInt(br.readLine());
+
+        while (N --> 0) {
+            int X = Integer.parseInt(br.readLine());
+            print(X);
+
+        }
+        br.close();
+    }
+    static void print(int X){
+        if(X == 0){
+            if(pq.isEmpty())
+                System.out.println(0);
+            else
+                System.out.println(pq.poll());
+        }else{
+            pq.add(X);
+        }
+    }
+}
